35125a1cd61fea20c6a01af5ea432180976fd9d6
Joseph Jung <joseph.jung@icloud.com>
2019-08-14 17:54:57 -0600

    Initial Commit

---
+++ CapturingPhotoSwift/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  CapturingPhotoSwift
+//
+//  Created by Joseph Jung on 8/14/19.
+//  Copyright © 2019 Joseph Jung. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
